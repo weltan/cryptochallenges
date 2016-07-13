126a87776b88deb27ef6005448fc08b4fbfeb12d
weltan <ken@pkcsecurity.com>
2016-07-12 18:44:22 -0700

    set 1 challenge 5

--- set1/C5_repeatingXOR/repeat_xor.go
+++ set1/C5_repeatingXOR/repeat_xor.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/weltan/cryptochallenges/utils"
 )
 
+const stanza = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+
 func main() {
-	fmt.Println("yo")
+	key := []byte("ICE")
+	stanzaBytes := []byte(stanza)
+
+	result := utils.KeyXOR(stanzaBytes, key)
+	fmt.Println(utils.BytesToHexString(result))
 }
